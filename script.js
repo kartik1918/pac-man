1e8036dd66f69ece106e128f2dd1bde6678f5602
Kartik Gupta <kartikgupta966@gmail.com>
2021-12-14 10:52:35 +0530

    added basic functionality

--- script.js
+++ script.js
@@ -45,14 +45,52 @@ function createBoard() {
            sq[i].classList.add('pac-dot');
        } else if (layout[i] === 1) {
            sq[i].classList.add('wall');
+       } else if (layout[i] === 2) {
+           sq[i].classList.add('ghost-lair')
        } else if (layout[i] === 3) {
             sq[i].classList.add('power-pellet');
        }
     }
-    console.log(squares.length);
-
 }
 
-console.log(layout.length);
-
 createBoard();
+
+//starting position of pacman
+let pacmanCurrentIdx = 490;
+
+sq[pacmanCurrentIdx].classList.add('pacman');
+
+function control(e) {
+    sq[pacmanCurrentIdx].classList.remove('pacman')
+  switch (e.keyCode) {
+      case 40:
+          if (!sq[pacmanCurrentIdx + width].classList.contains('ghost-lair') &&
+              !sq[pacmanCurrentIdx + width].classList.contains('wall') &&
+              pacmanCurrentIdx + width < width * width) pacmanCurrentIdx += width;
+          sq[pacmanCurrentIdx].classList.add('pacman');
+          console.log('pressed down');
+          break;
+      case 38:
+          if (!sq[pacmanCurrentIdx - width].classList.contains('ghost-lair') &&
+              !sq[pacmanCurrentIdx - width].classList.contains('wall') &&
+              pacmanCurrentIdx - width >= 0) pacmanCurrentIdx -= width;
+          sq[pacmanCurrentIdx].classList.add('pacman');
+          console.log('pressed up');
+          break;
+      case 37:
+          if (!sq[pacmanCurrentIdx-1].classList.contains('ghost-lair') &&
+              !sq[pacmanCurrentIdx-1].classList.contains('wall') &&
+              pacmanCurrentIdx % width !== 0) pacmanCurrentIdx -= 1;
+          console.log('pressed left');
+          break;
+      case 39:
+          if (!sq[pacmanCurrentIdx+1].classList.contains('ghost-lair') &&
+              !sq[pacmanCurrentIdx+1].classList.contains('wall') &&
+              pacmanCurrentIdx % width < width-1) pacmanCurrentIdx += 1;
+          console.log('pressed right');
+          break;
+  }
+  sq[pacmanCurrentIdx].classList.add('pacman');
+}
+
+document.addEventListener('keyup', control);
